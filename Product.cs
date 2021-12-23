dbd74dd8ff3ad374fe732a7507e07d7618ec725b
NataliaPri <77389496+NataliaPri@users.noreply.github.com>
2021-12-23 18:11:23 +0300

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp6
+{
+    public class Product
+    {
+        public string Item { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp6
+{
+    public class User
+    {
+        public string Person { get; set; }
+        public int Age { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
